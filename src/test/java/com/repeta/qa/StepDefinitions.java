2739ad2eb2f1a2c5f9792aed22bcc62b68effcd6
Ivan Repeta <ivan.repeta.work@gmail.com>
2020-12-15 20:39:11 +0200

    Initial commit

---
+++ src/test/java/com/repeta/qa/StepDefinitions.java
@@ -0,0 +1,12 @@
+package com.repeta.qa;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static org.junit.Assert.*;
+
+public class StepDefinitions {
+
+
+}
